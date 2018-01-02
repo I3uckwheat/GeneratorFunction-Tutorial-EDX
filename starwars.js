545c6047eeb976fda0709de378176aa101d7b8d4
Briggs Elsperger <I3uckwheat@gmail.com>
2018-01-01 19:22:25 -0600

    Adds generator function

--- starwars.js
+++ starwars.js
@@ -1,3 +1,7 @@
+document.getElementById("button").addEventListener("click", () => {
+  run(gen).catch(err => alert(err.message));
+})
+
 function run(genFunc){
   const genObject = genFunc();
 
@@ -13,4 +17,27 @@ function run(genFunc){
   } catch (ex){
     return Promise.reject(ex);
   }
+}
+
+function *gen(){
+  const filmNumber = document.getElementById("input");
+  if(filmNumber.value > 7 || filmNumber.value < 1) {
+    throw new Error("Invalid Input - Enter a number between 1 and 7");
+  }
+
+  const filmResponse = yield fetch("https://swapi.co/api/films/" + filmNumber.value);
+  const film = yield filmResponse.json();
+
+  const characters = film.characters;
+
+  let characterString = "Characters: <br>";
+
+  for(let i=0; i < characters.length; i++){
+    const tempCharacterResponse = yield fetch(characters[i]);
+    const tempCharacter = yield tempCharacterResponse.json();
+    characterString += tempCharacter.name + "<br>";
+  };
+
+  document.getElementById("filmsText").innerHTML = "Film: <br>" + film.title;
+  document.getElementById("peopleText").innerHTML = characterString;
 }
